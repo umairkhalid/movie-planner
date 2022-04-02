55a086ebe2d6f2bef5721c43046fb4f567ac7db3
Leo Wong <leoelicos@gmail.com>
2022-04-02 16:26:37 +1100

    Create getMovieInfo.js
    GIVEN a query string in input#searchBox,
    WHEN #searchBox is typed with "Enter" or WHEN #searchBtn" is clicked
    THEN append the results to the body

---
+++ assets/js/getMovieInfo.js
@@ -0,0 +1,67 @@
+// this document contains the necessary elements to
+// GIVEN user wants to see 5 movie results
+// WHEN they click Enter in the search box, or when they click the submit button
+// THEN an API call to OMDB will fetch search results
+
+// user can search by either clicking the submit button (magnifying glass)
+var buttonEl = document.getElementById('searchBtn');
+buttonEl.addEventListener('click', (e) => {
+	e.preventDefault();
+	performSearch();
+});
+
+// user can also search by typing enter button while inside the search field
+var inputEl = document.getElementById('searchBox');
+inputEl.addEventListener('keypress', (e) => {
+	if (e.key === 'Enter') {
+		e.preventDefault();
+		performSearch();
+	}
+});
+
+// function to search for movies
+function performSearch() {
+	updateQueryString();
+	callOMDB();
+}
+
+// function to store query string in global object 'headers'
+function updateQueryString() {
+	headers.s = searchBox.value;
+}
+var headers = {
+	apikey: '630b3ac4',
+	type: 'movie',
+	s: 'titanic',
+};
+
+const movieInfo = [];
+function callOMDB() {
+	fetch('http://www.omdbapi.com/?' + 'apikey=' + headers.apikey + '&type=' + headers.type + '&s=' + headers.s + '&r=json&page=1')
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			clearMovieInfo();
+			for (var i = 0; i < data.Search.length; i++) {
+				movieInfo.push({ title: data.Search[i].Title, type: data.Search[i].Type, year: data.Search[i].Year, imdbID: data.Search[i].imdbID, poster: data.Search[i].Poster });
+			}
+
+			// example render function
+			var ul = document.createElement('ul');
+			for (var i = 0; i < movieInfo.length; i++) {
+				var li = document.createElement('li');
+				li.innerHTML = `<span class="movieResultTitle">${movieInfo[i].title}</span><span class="movieResultYear">(${movieInfo[i].year})</span><img class="movieResultPoster" src="${movieInfo[i].poster}">`;
+				ul.appendChild(li);
+			}
+
+			var bodyEl = document.querySelector('body');
+			bodyEl.appendChild(ul);
+		});
+}
+
+function clearMovieInfo() {
+	while (movieInfo.length > 0) {
+		movieInfo.pop();
+	}
+}
