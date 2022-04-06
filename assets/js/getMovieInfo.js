9884767fe41c95b37c2f3497a28dd3a892f3ae4c
Leo Wong <leoelicos@gmail.com>
2022-04-06 21:47:01 +1000

    JS Comments for getMovieInfo.js

--- assets/js/getMovieInfo.js
+++ assets/js/getMovieInfo.js
@@ -1,9 +1,23 @@
-// this document contains the necessary elements to
-// GIVEN user wants to see 5 movie results
-// WHEN they click Enter in the search box, or when they click the submit button
-// THEN an API call to OMDB will fetch search results
+/* 
+getMovieInfo.js
+This document contains the necessary elements to search for movies on OMDB and render them, and also render recent searches
+Copyright 2022 MOOVEE Team
 
+ Acceptance criteria:
+ GIVEN user wants to see 5 movie results, 
+ WHEN they click Enter in the search box, 
+ or WHEN they click the submit button,
+ THEN an API call to OMDB will fetch search results and render them to the page
+
+ GIVEN user wants to see a history of search results,
+ WHEN they click the Recent Searches button,
+ THEN a list of recent searches will be available
+ WHEN they click on a list item
+ THEN the search bar will show the list item, and a search will be run automatically
+ */
+// handle for container to hold results 
 const resultGrid = document.getElementById("resultGrid");
+// handle for search button
 var recentSearchBtn = document.getElementById("searchButton")
 
 // user can search by either clicking the submit button (magnifying glass)
@@ -24,41 +38,50 @@ inputEl.addEventListener("keypress", (e) => {
 
 // function to search for movies
 function performSearch() {
-	// get localStorage
+	// get localStorage JSON object and parse it to an array
 	var movieHistory = JSON.parse(localStorage.getItem("movieList")) || [];
 
-	// set localStorage
+	// set localStorage for the value inside search box
 	var movie = { title: inputEl.value };
 	movieHistory.push(movie);
 
 	localStorage.setItem("movieList", JSON.stringify(movieHistory));
 
+	// render list from local storage to the page
 	renderRecentSearches();
 
+	// grab the value inside search box and update it to local variable
 	updateQueryString();
+
+	// search OMDB with the search box query, and also render it to the page
 	callOMDB();
 }
 
+// function to run on startup
 function init() {
-	
-
-	renderRecentSearches(); // run on page load
+	// render any local storage files to the dropdown menu
+	renderRecentSearches();
 }
+// run on startup
 init();
 
-// TODO: a function to render recent searches. Run this function here, and also on startup.
-// TODO: This function will get list from local storage, delete all the children from the dropdown ul, and render the recent search results from scratch;
+// function to render recent searches. Run this function when button is pressed, and also on startup.
+// this function will get list from local storage, delete all the children from the dropdown ul, and render the recent search results from scratch;
 function renderRecentSearches() {
-	var movieSearch = document.querySelector("#movieList");
-	var movieList = JSON.parse(localStorage.getItem("movieList")) || [];
+	console.log('reder');
+	// grab the container for the dropdown list of local history
+	var movieSearch = document.querySelector('#movieList');
+	// grab the local history and parse it into an array
+	var movieList = JSON.parse(localStorage.getItem('movieList')) || [];
+	console.log(movieList);
 
-	
-
-	movieSearch.innerHTML = "";
-	for (var i = 0; i <movieList.length; i++) {
+	// delete all the children from the dropdown ul
+	movieSearch.innerHTML = '';
+	// loop through list of locally stored titles and render the recent search results from scratch
+	for (var i = 0; i < movieList.length; i++) {
 		var movieName = movieList[i].title;
 
-		var li = document.createElement("li");
+		var li = document.createElement('li');
 		li.textContent = movieName;
 
 		movieSearch.appendChild(li);
@@ -74,7 +97,9 @@ function renderRecentSearches() {
 	});
 }
 
+// add a listener on startup to render recent searches
 recentSearchBtn.addEventListener("click",function(){
+	console.log("hello world")
 	renderRecentSearches();
 }) 
 	
@@ -100,6 +125,8 @@ var genres = [];
 var actors = [];
 var plots = [];
 var ratings = [];
+// function that calls OMDB to get metadata of a movie, given a query string
+// function also delegates a render function for the metadata
 function callOMDB() {
 	fetch("https://www.omdbapi.com/?" + "apikey=" + headers.apikey + "&type=" + headers.type + "&s=" + headers.s + "&page=1")
 		.then(function (generalSearchResult) {
@@ -107,6 +134,7 @@ function callOMDB() {
 		})
 
 		.then(function (data1) {
+			// console.log(data1);
 
 			// clear metadata stack
 			clearMovieInfo();
@@ -138,15 +166,19 @@ function callOMDB() {
 			}
 		})
 		.then(() => {
+			console.log("Rendering function:");
 			setTimeout(renderFunction, 2000);
+			console.log("Adding button listeners:");
 			setTimeout(addListeners, 2000);
 			// console.log('Rendering video:');
 			// setTimeout(dummyRender, 2000);
 		}); // I have allowed 1 second of thinking time for the computer
 }
-
+// function to render the metadata to the page
 function renderFunction() {
+	// clear the result container before rendering
 	resultGrid.innerHTML = "";
+	// loop through the length of the movies returned from OMDB search
 	for (var i = 0; i < posters.length; i++) {
 		resultGrid.innerHTML += `
 					<div class= "resultContainer">
